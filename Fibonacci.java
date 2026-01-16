7c48a10b2e0aa307a8ccd0a15cd394bde7e7687d
kSkid20 <232664731+kSkid20@users.noreply.github.com>
2026-01-16 11:55:47 -0500

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,9 @@
 public class Fibonacci {
 
+//Uses recursion for the Fibonacci method
+// @param n the term position which starts at 0
+// @return the Fibonacci value, which is represented my n
+
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
